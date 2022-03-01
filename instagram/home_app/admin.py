02b526825d03960a98d4db156bd93a8b257d99bc
rafiqulislam21 <ribijoy19@gmail.com>
2022-03-01 23:54:52 +0100

    model added in admin

--- instagram/home_app/admin.py
+++ instagram/home_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from home_app.models import UserPost, SaveUserPost, Comment
+from home_app.models import UserPost, SaveUserPost, Comment, LikeUserPost
 
 # Register your models here.
 admin.site.register(UserPost)
 admin.site.register(Comment)
 admin.site.register(SaveUserPost)
+admin.site.register(LikeUserPost)
